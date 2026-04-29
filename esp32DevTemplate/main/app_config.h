d4a0854c130c9afe9b11f60ad87185b8bb8401c9
rhit-Anar <112890439+rhit-Anar@users.noreply.github.com>
2026-04-29 15:36:06 -0400

    BLE and hello word added

---
+++ esp32DevTemplate/main/app_config.h
@@ -0,0 +1,5 @@
+//this where you diable and able methods here. 0/1 
+#pragma once
+
+#define USE_HELLO_WORLD 0
+#define USE_BLE 1
---
+++ esp32DevTemplate/main/main.c
@@ -0,0 +1,29 @@
+//Author: Anar Amartuvshin
+#include <stdio.h>
+#include "app_config.h"
+
+#if USE_HELLO_WORLD
+#include "hello_world/hello_world.h"
+#endif
+
+#if USE_BLE
+#include "nimBLE_gatt_server_setup.h"
+#endif
+void ble_gatt_main(void);
+void app_main(void)
+{
+    printf("ESP32 Starter Project\n");
+//Toggle Mode for hello wolrd, to remove it you can go to the app_config.h file and set the hello world to 0. T
+#if USE_HELLO_WORLD
+    hello_world_run();
+#endif
+//=============================================================================================================
+
+//BLE_gatt_server setup----------------------------------------------------------------------------------------
+#if USE_BLE
+    ble_gatt_main();
+    printf("Calling the ble gatt server!");
+#endif
+//=============================================================================================================
+
+}
